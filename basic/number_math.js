765217c585cdb499875782c867cb856989bdf917
Muhammad-Ahmerr <126159056+Muhammad-Ahmerr@users.noreply.github.com>
2024-01-02 17:02:39 +0000

    numbers ansd math( )

---
+++ basic/number_math.js
@@ -0,0 +1,17 @@
+//This strictly define that would be number not other
+const num=new Number(1235)
+// console.log(num);
+
+const num2=123.747
+
+// console.log(num2.toFixed(2));
+// console.log(num2.toLocaleString());
+// console.log(num2.toPrecision(4));
+// console.log(Math.round(num2));
+// console.log(Math.floor(( Math.random()*3) +1));
+
+//find numbers between max and min
+const min=10;
+const max=20;
+
+console.log((Math.floor(Math.random()*(max-min +1))+min));
