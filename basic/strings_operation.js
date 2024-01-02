9907200895b36b25e721f027f4fff8e9b0d8d23c
Muhammad-Ahmerr <126159056+Muhammad-Ahmerr@users.noreply.github.com>
2024-01-02 15:23:43 +0000

    string operations

---
+++ basic/strings_operation.js
@@ -0,0 +1,77 @@
+const stringOne='Ahmer@gmail.com';
+const stringTwo='ahsan@gmail.com';
+const a = 'RESERVE'; // With accents, lowercase
+const b = 'RESERVE';
+
+// console.log(stringOne.length);
+// console.log(stringOne.charAt(2));
+// console.log(stringOne[1]);
+// console.log(stringOne.charCodeAt(14));
+// console.log(stringOne.endsWith('.com'));
+
+//the ending index wii be counted
+// console.log(stringOne.endsWith('.co m',10));
+// console.log(stringOne.includes('comm'));
+// console.log(stringOne.indexOf('m'));
+    //    console.log(stringOne.lastIndexOf('.com'));
+    // console.log("canal".lastIndexOf("a"));
+            // console.log(stringOne.localeCompare(stringTwo));
+            // console.log(stringOne.localeCompare(stringTwo));
+// Expected output: 1
+
+// console.log(a.match(b));
+
+
+// console.log(stringOne.concat(stringOne,stringTwo,'as'));
+// console.log(stringOne.endsWith());
+
+// console.log(stringOne.charAt(2));
+// console.log(stringOne.endsWith('.com'));
+// console.log(stringOne.includes('com'));
+// console.log(stringOne.indexOf('hmer'));
+// console.log(stringOne.lastIndexOf('h'));
+// console.log(stringOne.localeCompare(stringTwo));
+
+let sentence='The rain in the water is strain in RAin'
+
+//this return the first index in all the sentence
+// console.log(sentence.match('ain'));
+
+//this return the globle array of all same case element
+// console.log(sentence.match(/ain/g));
+
+//this will return all globle array without case-sensitivity
+// console.log(sentence.match(/ain/gi));
+
+//This will give you all the index with array        
+// let reg=sentence.matchAll(/ain/gi)
+// console.log(...reg);
+
+// console.log(stringOne.padStart(20, ' '));
+// console.log(stringOne.padEnd(20, '?'));
+// console.log(stringOne.repeat(2));
+// console.log(stringOne.replace('com','dom'));
+
+
+// let req='com'
+// console.log(stringOne.replace(req,'DOm'));
+// console.log(stringOne.search('h'));
+// console.log(stringOne.slice(2,10));
+
+//this returns the array of charaters
+// console.log(stringOne.split(''));
+
+
+// let fruit='ahmer,altaf,asif,basit'
+// console.log(fruit.split(',',3));
+
+//the return boolean
+// console.log(stringOne.startsWith('h',2));
+
+// console.log(stringOne.substring(2,6));
+
+// console.log(stringOne.toLocaleUpperCase());
+
+// console.log(stringOne.toLowerCase('sA'));
+
+// console.log(stringOne.valueOf());
--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,46 @@
-console.log('ss');
+// console.log('ss');
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null>0);
+
+
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined==0);
+
+// const myFunction= function(){
+//     console.log(typeof myFunction);
+// }
+
+// myFunction();
+
+//Stack(Premitive)   
+
+
+// let myName='ahmer';
+// let anotherName=myName;
+
+// anotherName='ahmer2'
+// console.log(anotherName);
+
+// _________________Heap(Non-Premitive)
+
+// let object1={
+//     name:'ahmer',
+//     email: 'ahmer@gmail.com'
+// }
+
+// let object2=object1;
+
+// object2.email='ahmer2'
+// console.log(object1.email);
+// console.log(object2.email);
+
+// _______________
+
+//   ary   =['sss','as','wqw'];
+//  console.log(typeof ary);
+
+const name1='ahmer';
+const name2=' altaf';
+console.log(`Hello my first name= ${name1.toUpperCase()} and second name is ${name2}`);
