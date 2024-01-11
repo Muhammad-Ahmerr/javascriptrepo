e1276ef56725736575ec1c8d7db37fad5ce1acb6
Muhammad-Ahmerr <126159056+Muhammad-Ahmerr@users.noreply.github.com>
2024-01-11 11:04:40 +0000

    All Loops

---
+++ basic/for_loop.js
@@ -0,0 +1,61 @@
+// let nymber=2
+// for (let index = 1; index <= 10; index++) {
+//     console.log(`${nymber} x ${index} = ${nymber*index}`);
+    
+// }
+
+
+// for (let index = 1; index <= 10; index++) {
+//     console.log(`outer loop ${index}`);
+//     for (let j = 1; j <= 10; j++) {
+//             console.log(`inner  loop ${j}`);  
+//     }
+    
+// }
+
+
+// for (let index = 1; index <= 10; index++) {
+//     console.log(`outer loop ${index}`);
+//     for (let j = 1; j <= 10; j++) {
+//       console.log(`${index} x ${j}=${index*j}`);  
+//     }
+    
+// }
+
+const myArr=['ahmer','tayyad','umair','ahmad']
+
+for (let index = 0; index < myArr.length; index++) {
+    if(index==1){
+        //1 bar maffi milti hai
+        continue
+    }
+    if(index==2)
+    {
+        break;
+    }
+    const element = myArr[index];
+    // console.log(element);9
+    
+}
+
+let array=['batman','superman','aflatoon','spiderman']
+let index=0
+// while (index<array.length) {
+//     console.log(array[index]);
+//     index++;
+    
+// }
+
+// do {
+//     console.log(array[index]);
+//     index++
+    
+// } while (index<array.length);
+
+
+// let index1=11
+// do {
+//     console.log(index1);
+//     index++
+    
+// } while (index1<=10);
